a9dcf6c37757d32204c086a848c460b32f44c8b2
alex-physics1 <alex.gsone@gmail.com>
2024-06-19 18:28:24 -0300

    Otimização do código!

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 function playRockPaperScissor(player1, player2){
           
-    if(player1 === "Pedra" && player2 === "Tesoura"){
+    if((player1 === "Pedra" && player2 === "Tesoura") || 
+            (player2 === "Pedra" && player1 === "Papel") || 
+                    (player1 === "Tesoura" && player2 === "Papel")){
         return "Jogador 1 venceu!"
 
-    }else if(player1 === "Pedra" && player2 === "Papel"){
-        return "Jogador 2 venceu!"
-
-    }else if(player2 === "Pedra" && player1 === "Tesoura"){
-        return "Jogador 2 venceu!"
-
-    }else if(player2 === "Pedra" && player1 === "Papel"){
-        return "Jogador 1 venceu!"
-
-    }else if(player1 === "Tesoura" && player2 === "Papel"){
-        return "Jogador 1 venceu!"
-
-    }else if(player2 === "Tesoura" && player1 === "Papel"){
+    }else if((player1 === "Pedra" && player2 === "Papel")|| 
+            (player2 === "Pedra" && player1 === "Tesoura") || 
+                    (player2 === "Tesoura" && player1 === "Papel")){
         return "Jogador 2 venceu!"
 
     }else{
         return "Empate!"
-    }    
+    }
 }
